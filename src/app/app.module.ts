ff485df6bbbd403d792ffa557ced54bcdbc5cf62
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-01 22:11:50 -0500

    app.module of master branch equals to app.module develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { NgModule, ErrorHandler }                   from '@angular/core';
-import { BrowserModule }                            from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp }                                    from './app.component';
+import { MyApp } from './app.component';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
-import { StatusBar }    from '@ionic-native/status-bar';
+import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { PendientesComponent } from "../pages/pendientes/pendientes.component";
