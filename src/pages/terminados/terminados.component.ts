addee6a08c209b9f9eeb5400ed37dfba077987de
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-12-12 22:18:27 -0500

    Basic functionality of the project ready

--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,4 +1,7 @@
-import  { Component, OnInit } from "@angular/core";
+import { Component, OnInit }    from '@angular/core';
+import { ListaDeseosService }   from '../../app/services/lista-deseos.service';
+import { NavController }        from "ionic-angular";
+import { DetailComponent }      from '../detail/detail.component';
 
 @Component({
     selector:'app-terminado',
@@ -7,11 +10,16 @@ import  { Component, OnInit } from "@angular/core";
 
 export class TerminadosComponent implements OnInit{
 
-    constructor(){
-        
-    }
+    constructor( private _listaDeseosService:ListaDeseosService,
+        private _navController:NavController ){
+
+}
 
     ngOnInit(){
 
+    }
+
+    showDetail(lista, idx){
+    this._navController.push( DetailComponent, { lista, idx });
     }
 }
